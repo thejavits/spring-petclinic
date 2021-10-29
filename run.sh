c8971e29d6dccba343fde913ddc1574d490a45cb
krishna <53662491+thejavits@users.noreply.github.com>
2021-10-29 11:30:52 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 image_name="krishnademo.jfrog.io/default-docker-local/krishna-pet-clinic"
 /usr/local/bin/docker pull $image_name
 /usr/local/bin/docker rm $(/usr/local/bin/docker stop $(/usr/local/bin/docker ps -a -q --filter ancestor=krishnademo.jfrog.io/default-docker-local/krishna-pet-clinic --format="{{.ID}}"))
-/usr/local/bin/docker run -d -p 8080:8088 $image_name
+/usr/local/bin/docker run -d -p 8088:8080 $image_name
