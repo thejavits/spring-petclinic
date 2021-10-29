deee84d2473a292214ff6ac67ec79bc1ce3b3db2
krishna <53662491+thejavits@users.noreply.github.com>
2021-10-29 11:23:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 # Requires docker engine 17.05 
 image_name="krishnademo.jfrog.io/default-docker-local/krishna-pet-clinic"
 /usr/local/bin/docker pull $image_name
+/usr/local/bin/docker rm $(/usr/local/bin/docker stop $(/usr/local/bin/docker ps -a -q --filter ancestor=krishnademo.jfrog.io/default-docker-local/krishna-pet-clinic --format="{{.ID}}"))
 /usr/local/bin/docker run -d -p 8080:8088 $image_name
